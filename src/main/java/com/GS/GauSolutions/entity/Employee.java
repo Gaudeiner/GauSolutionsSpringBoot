340225334d0d6804f13feb5ae30bd1cf3de69cc3
Gaudeiner <gaudeiner@hotmail.com>
2022-09-18 10:06:29 -0500

    Modelamiento del modelamiento base de las entidades

---
+++ src/main/java/com/GS/GauSolutions/entity/Employee.java
@@ -0,0 +1,74 @@
+package com.GS.GauSolutions.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @ManyToOne
+    private Enterprise enterprise;
+
+    private Date updateAt;
+    private Date createAt;
+
+    public Employee(){
+
+    }
+
+    public Employee(long id, String email, Enterprise enterprise, Date updateAt, Date createAt) {
+        this.id = id;
+        this.email = email;
+        this.enterprise = enterprise;
+        this.updateAt = updateAt;
+        this.createAt = createAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
---
+++ src/main/java/com/GS/GauSolutions/entity/Enum_RolName.java
@@ -0,0 +1,9 @@
+package com.GS.GauSolutions.entity;
+
+import javax.persistence.Table;
+
+@Table
+public enum Enum_RolName {
+    Admin,
+    Operario,
+}
